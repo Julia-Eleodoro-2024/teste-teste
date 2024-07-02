683d305c1fa0b4a3fa1d26297fed99ff02a3c72e
julia-kw <148871757+julia-kw@users.noreply.github.com>
2024-07-02 11:47:36 -0300

    Add files via upload

---
+++ projeto_jdbc_julia/src/main/java/com/eleodoro/dao/lojaPojo.java
@@ -0,0 +1,50 @@
+package com.eleodoro.dao;
+
+public class lojaPojo {
+    private String cliente;
+    private int id;
+    private String marca;
+   
+        
+    
+    public lojaPojo() {
+    
+    }
+
+
+
+    public String getCliente() {
+        return cliente;
+    }
+
+
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+}
---
+++ projeto_jdbc_julia/src/main/java/com/eleodoro/modelo/loja.java
@@ -0,0 +1,80 @@
+package com.eleodoro.modelo;
+
+public class loja {
+  private String cliente;
+  private int id;
+  private String marca;
+  
+
+
+    public loja() {
+
+    }
+
+    public loja(String cliente, int id, String marca) {
+        this.cliente = cliente;
+        this.id = id;
+        this.marca = marca;
+        
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+     public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        loja other = (loja) obj;
+        if (cliente == null) {
+            if (other.cliente != null)
+                return false;
+        } else if (!cliente.equals(other.cliente))
+            return false;
+        if (id != other.id)
+            return false;
+        if (marca == null) {
+            if (other.marca != null)
+                return false;
+        } else if (!marca.equals(other.marca))
+            return false;
+        return true;
+    }
+
+}
